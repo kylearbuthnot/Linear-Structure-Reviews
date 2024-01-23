7085143f4b29d39a9e3bea9f5bae1339aad82f54
Kyle Arbuthnot <arbuthnotkyle@gmail.com>
2024-01-23 15:42:02 -0600

    finally got debugger working, when compiling use -g tag before the .cpp file we want to debug.

--- Linear Structure/stack.cpp
+++ Linear Structure/stack.cpp
@@ -5,25 +5,40 @@ using namespace std;
 
 //Function to determine if parenthesis are valid. 
 bool isValid(string s) {
-        //stack to help with determining the validity.
-        stack<char> sChar;
-        for(int i = 0; i < s.size(); i++){
+    //stack to help with determining the validity.
+    stack<char> sChar;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
             sChar.push(s[i]);
         }
-        if(sChar.empty()){
-            return true;
+        else if (s[i] == ')') {
+            if (sChar.empty()) {
+				return false;
+			}
+            else {
+                sChar.pop();
+            }
         }
-        return false;
     }
+
+    //If the stack is empty, then the parenthesis are valid. Otherwise return false.
+    if (sChar.empty()) {
+        return true;
+    }
+    return false;
+}
 
 int main() {
     //string to test with.
-    string myString = "()()";
-    cout << "Breakpoint hit..";
-    //Test the string.
-    if(isValid(myString)){
-        cout << myString << " is a valid string of parenthesis.";
-    }
+    string myString = ")(";//false
+    cout << isValid(myString) << endl;
+    string myString2 = "((())";//false
+    cout << isValid(myString2) << endl;
+    string myString3 = "()";//true
+    cout << isValid(myString3) << endl;
+    string myString4 = "(a)((b)((c)(d)))";//true
+    cout << isValid(myString4) << endl;
+    
 
     return 0;
 }
